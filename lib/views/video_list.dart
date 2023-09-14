0f1d30bf13b359fdc7391e9b27f7e555bff3fc3b
mahdiramazani <mahdiramazani1281@gmail.com>
2023-09-14 12:49:39 +0330

    finish

--- lib/views/video_list.dart
+++ lib/views/video_list.dart
@@ -104,3 +104,67 @@ class _VideoListState extends State<VideoList> {
     )));
   }
 }
+
+
+// import 'package:flutter/material.dart';
+// import 'package:video_player/video_player.dart';
+
+// void main() => runApp(const VideoList());
+
+// /// Stateful widget to fetch and then display video content.
+// class VideoList extends StatefulWidget {
+//   const VideoList({super.key});
+
+//   @override
+//   _VideoListState createState() => _VideoListState();
+// }
+
+// class _VideoListState extends State<VideoList> {
+//   late VideoPlayerController _controller;
+
+//   @override
+//   void initState() {
+//     super.initState();
+//     _controller = VideoPlayerController.networkUrl(Uri.parse(
+//         'https://flutter.github.io/assets-for-api-docs/assets/videos/bee.mp4'))
+//       ..initialize().then((_) {
+//         // Ensure the first frame is shown after the video is initialized, even before the play button has been pressed.
+//         setState(() {});
+//       });
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       title: 'Video Demo',
+//       home: Scaffold(
+//         body: Center(
+//           child: _controller.value.isInitialized
+//               ? AspectRatio(
+//                   aspectRatio: _controller.value.aspectRatio,
+//                   child: VideoPlayer(_controller),
+//                 )
+//               : Container(),
+//         ),
+//         floatingActionButton: FloatingActionButton(
+//           onPressed: () {
+//             setState(() {
+//               _controller.value.isPlaying
+//                   ? _controller.pause()
+//                   : _controller.play();
+//             });
+//           },
+//           child: Icon(
+//             _controller.value.isPlaying ? Icons.pause : Icons.play_arrow,
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+
+//   @override
+//   void dispose() {
+//     super.dispose();
+//     _controller.dispose();
+//   }
+// }
